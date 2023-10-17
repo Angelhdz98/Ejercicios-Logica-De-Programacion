8688cba728078718869e179561f7804da77fc293
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2023-10-17 01:03:13 -0600

    me equivoqué al poner el nombre de un archivo y terminé el ejercicio 5 de programación de adivinar el numero secreto

---
+++ Ejercicio de Lógica de Programación 5/ejercicio5.js
@@ -0,0 +1,23 @@
+let valorSecreto= parseInt(23);
+
+let valor;
+let i=0;
+  while (i==0) {
+    valor = parseInt(prompt("Ingresa un valor, a ver si adivinas el numero más iconico de todos del 1 al 100 (secreto)"));
+    if (!isNaN(valor) && Number.isInteger(valor) && valor==valorSecreto) {
+      i++;
+    }else if(!isNaN(valor) && Number.isInteger(valor) && valor!=valorSecreto && valor>1 && valor<100){
+        console.log("lo siento, ", valor, " no es el valor secreto");
+    } 
+    else if(valor<1|| valor>100){
+        console.log("El valor esta fuera del rango solicitado, por favor ingresa un número entero entre el 1 y el 100");
+    } 
+    else {
+      console.log("¡Eso no es un número entero válido! Por favor ingresa un número entero entre el 1 y el 100");
+    }
+
+  }
+
+  console.log("bien hecho has acertado ", valorSecreto, " es el numero secreto" );
+
+
